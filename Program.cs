b75cc4e679df362e2e383688bf5169ad8afb7719
Tuan Duong Manh <dmtuan1995@gmail.com>
2022-11-09 14:48:07 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+using ContosoUniversity.Data;
+using Microsoft.EntityFrameworkCore;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<SchoolContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+var app = builder.Build();
+using var scope = app.Services.CreateScope();
+SchoolContext context = scope.ServiceProvider.GetRequiredService<SchoolContext>();
+DbInitializer.Initialize(context);
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
